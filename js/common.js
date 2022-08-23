0f03d4d25d6213f5a8406820c908704707b5ab8d
bossung13 <107093009+bossung13@users.noreply.github.com>
2022-08-23 11:44:54 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,87 +1,54 @@
+// 네비게이션 슬라이드
 $('#gnb').on('mouseenter', function() {
-    $('#headerWrap').addClass('on')
+  $('#header').addClass('on')
 })
-$('#headerWrap').on('mouseleave', function() {
-    $(this).removeClass('on')
+$("#header").on('mouseleave', function() {
+  $('#header').removeClass('on')
 })
 
 
+$("#product h2").fadeIn({ opacity: 0 }, 1000)
+$("#product p").delay(400).animate({ opacity: 1 }, 2000)
 
-
-// 배너슬라이더
-let sList = $('#sliderList');
-let liLength = $('#sliderList li').length;
-let num = 0;
-let state = 1;
-let prevSlider = function(){
-    num--;
-    if(num == -1){
-        num = liLength-1;
-        sList.prepend($('li:last', sList))
-             .css({marginLeft:"-100%"})
-             .animate({marginLeft:0}, 500, function(){
-                for(let i=0; i<liLength-1; i++){
-                    sList.prepend($('li:last', sList))
-                }
-                sList.css({marginLeft:`-${(liLength-1)*100}%`})
-                state = 1;
-             })
-    }
-    else{
-        sList.not(':animated').animate({marginLeft:'+=100%'}, function(){
-            state = 1;
-        })
-    }
-    $("#sliderBtn a").removeClass('active');
-    $("#sliderBtn a:eq("+ num +")").addClass('active')
+let n = 0;
+let sliding = function() {
+  n++;
+  if ( n==3 ) {
+    n = 0;
+    $('#sliderList').animate({ marginLeft: 0 })
+  }
+  else {
+    $('#sliderList').animate({ marginLeft: "-=100%" })
+  }
 }
-let nextSlider = function(){
-    num++; 
-    if( num == liLength ){
-        num = 0;
-        sList.append($('li:first', sList))
-             .css({marginLeft:`-${(liLength-2)*100}%`})
-             .animate({marginLeft:`-${(liLength-1)*100}%`}, 500, function(){
-                for(let i=0; i<liLength-1; i++ ){
-                    sList.append($('li:first', this))
-                }
-                sList.css({marginLeft:0})
-                state = 1;
-             })
-    }
-    else{
-        sList.not(':animated').animate({marginLeft:'-=100%'}, function(){
-            state = 1;
-        })  
-    }
-    $("#sliderBtn a").removeClass('active');
-    $("#sliderBtn a:eq("+ num +")").addClass('active')
-}
+let timer = setInterval(sliding, 4000);
 
-let timer = setInterval(nextSlider, 3000)
+$('#sliderBtn a').on('click', function(e) {
+  if (n == 2) {
+    
+  }
+  else {
+  
+  }
+  n = $(this).index()+1;
+  e.preventDefault();
+  let pos = $(this).index() * -$('.slider').width();
+  $('#sliderList').animate({ marginLeft: pos})
+  clearInterval(timer);
+  timer = setInterval(sliding, 3000)
+})
 
-$("#sliderBtn a").on('click', function(e) {
-    $("#sliderBtn a").removeClass('active');
-    $(this).addClass('active')
-    num = $(this).index();
-    sList.animate({ marginLeft: `${num*(-100)}%` }), 1000
+
+// 상품 슬라이드
+
+
+  $('.nextBtn').on('click', function(e) {
+    e.preventDefault();
+    $('.drinks:not(animated)').animate({ marginLeft: '-=300px'})
+  })
+  $('.prevBtn').on('click', function(e) {
+    e.preventDefault();
+    $('.drinks:not(animated)').animate({ marginLeft: '+=300px'})
   })
 
-$('#sliderBtn a').on('click', function(e){
-    num = $(this).index();
-    sList.animate({marginLeft: `${num*(-100)}%` })
-})
-$('#sliderBtn a').on('click', function(e){
-    e.preventDefault();
-    clearInterval(timer)
-    timer = setInterval(nextSlider, 3000)
-})
 
-
-// 제품슬라이더
-$("#sliderBtn a:first").addClass(`on`)
-$("#sliderBtn a").on('click', function(e) {
-  e.preventDefault();
-  $("#sliderBtn a").removeClass();
-  $(this).addClass('on')
-})
