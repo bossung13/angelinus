613f9504faa1b619e1012ecac2204ee1262d67de
bossung13 <107093009+bossung13@users.noreply.github.com>
2022-08-30 09:11:05 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -7,8 +7,8 @@ $("#header").on('mouseleave', function() {
 })
 
 
-$("#product h2").fadeIn({ opacity: 0 }, 1000)
-$("#product p").delay(400).animate({ opacity: 1 }, 2000)
+// $("#product h2").fadeIn({ opacity: 0 }, 1000)
+// $("#product p").delay(400).animate({ opacity: 1 }, 2000)
 
 let n = 0;
 let sliding = function() {
@@ -20,35 +20,52 @@ let sliding = function() {
   else {
     $('#sliderList').animate({ marginLeft: "-=100%" })
   }
+  $('#sliderBtn a').removeClass('on')
+  $("#sliderBtn a:eq("+ n +")").addClass('on')
 }
-let timer = setInterval(sliding, 4000);
+let timer = setInterval(sliding, 5000);
 
 $('#sliderBtn a').on('click', function(e) {
-  if (n == 2) {
-    
-  }
-  else {
-  
-  }
-  n = $(this).index()+1;
+  $('#sliderBtn a').removeClass('on');
+  $(this).addClass('on')
+  n = $(this).index();
   e.preventDefault();
   let pos = $(this).index() * -$('.slider').width();
   $('#sliderList').animate({ marginLeft: pos})
   clearInterval(timer);
-  timer = setInterval(sliding, 3000)
+  timer = setInterval(sliding, 5000)
 })
 
 
 // 상품 슬라이드
 
-
+  let proNum = 0;
   $('.nextBtn').on('click', function(e) {
     e.preventDefault();
-    $('.drinks:not(animated)').animate({ marginLeft: '-=300px'})
+    $('.drinks:not(:animated)').animate({ scrollLeft: '+=300px'})
+    proNum++;
+    if ( proNum == 3 ) {
+      proNum = 2;
+      return;
+    }
+    else {
+      $('#slidebottom div').animate({ left: '+=140'})
+    }
+    // console.log(proNum)
   })
   $('.prevBtn').on('click', function(e) {
+    proNum--
+    console.log(proNum)
     e.preventDefault();
-    $('.drinks:not(animated)').animate({ marginLeft: '+=300px'})
+    $('.drinks:not(:animated)').animate({ scrollLeft: '-=300px'})
+    if ( proNum == -1 ) {
+      proNum = 0;
+      return;
+    } else {
+      $("#slidebottom div").animate({ left: '-=140'})
+    }
   })
+
+
 
 
